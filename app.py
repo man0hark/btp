d6223f768145ce66e1d57fbf3ec26ce7ba386084
Manohar K <56685489+man0hark@users.noreply.github.com>
2021-12-15 16:13:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ import python_speech_features
 from matplotlib.figure import Figure
 from tensorflow.keras.models import load_model
 from flask import *
-from tensorflow.keras.models import Sequential
 import tensorflow as tf
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, Activation
-from tensorflow.keras.layers import LSTM
+
+
 app = Flask(__name__) 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('Home.html')
 @app.route('/success',methods=['POST'])
 def success():
     f = request.files['file']
@@ -28,15 +28,19 @@ def success():
     ms = np.array(mfcc_speech)
     ms = ms.reshape(ms.shape[0],ms.shape[1],1)
     y_pred = model1.predict(ms)
+    y_pred = y_pred.round()
     img = BytesIO()
+    plt.style.use('seaborn-muted')
+    plt.figure(figsize=(10,5))
     plt.subplot(2,1,1)
     plt.plot(data)
     plt.subplot(2,1,2)
     plt.plot(y_pred.round())
     plt.savefig(img, format='png',transparent=True)
+    plt.axis('off')
     plt.close()
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode('utf8')
     return render_template('plot.html', plot_url=plot_url)
 if __name__ == '__main__':  
-    app.run(debug = True) 
+    app.run(debug = True)
