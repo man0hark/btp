f7922b4aec6c83ba98e198efed1c5d2294e97343
Manohar K <56685489+man0hark@users.noreply.github.com>
2021-12-15 16:27:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from matplotlib.figure import Figure
 from tensorflow.keras.models import load_model
 from flask import *
 import tensorflow as tf
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
